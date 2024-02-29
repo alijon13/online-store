67a8932ad3be77c50700a2c5a1949d4063a84a37
alijon13 <alijonrasulov1337@gmail.com>
2024-02-29 19:19:23 +0500

    last

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,6 @@ import Home from "./Pages/Home/Home";
 import Contact from "./Pages/Contact/Contact";
 import About from "./Pages/About/About";
 import ProductById from "./Pages/ProductById/ProductById";
-import LogIn from "./Pages/Login/LogIn";
 import Layout from "./Layout/Layout";
 import Buket from "./Pages/Buket/Buket";
 
